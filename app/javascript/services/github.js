484c78184310ea493cb35f0e19b20b39ad2c77f5
Ketan Saxena <ketan.1806@gmail.com>
2016-08-31 10:17:06 +0530

    JSdocs added

--- app/javascript/services/github.js
+++ app/javascript/services/github.js
@@ -9,10 +9,20 @@ const API = {
 const PER_PAGE_RECORDS = 10;
 
 const GithubService = {
+  /**
+  * method to fetch Github users from thier API.
+  * @param {number} showSince: The integer ID of the last User that you've seen.
+  * @param {number} perPageRecords: number of records wanted in one result-set.
+  * @returns {Promise}
+  */
   getUsers: (showSince, perPageRecords) => {
     return http.performGet(GIT_BASE_URL + API.users,
       { showSince: showSince,  per_page: perPageRecords || PER_PAGE_RECORDS})
   },
+  /**
+  * method to return default per page records count.
+  * @returns {number}
+  */
   getPerPageRecordCount: () => PER_PAGE_RECORDS
 };
 
