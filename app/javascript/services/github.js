0ffa03d185c3cc71ab35d328b5ae6c0dbb26621a
Ketan Saxena <ketan.1806@gmail.com>
2016-08-31 09:55:46 +0530

    Update github.js

--- app/javascript/services/github.js
+++ app/javascript/services/github.js
@@ -1,8 +1,4 @@
 import http from '../mixins/restutils';
-import UI from '../mixins/ui';
-import Utility from '../mixins/basicUtils';
-import CONSTANTS from '../constants/app-constant';
-import Authorization from './authorization';
 
 const GIT_BASE_URL = 'https://api.github.com/';
 
